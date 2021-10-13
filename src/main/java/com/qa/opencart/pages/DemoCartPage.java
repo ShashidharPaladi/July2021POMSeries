bed5c9d88f978cedebd1c16e7a1ffee8fbe16784
ShashidharPaladi <shashidharpaladi@Shashidhars-MacBook-Pro.local>
2021-10-13 23:43:32 +0530

    adding demoCartPage

---
+++ src/main/java/com/qa/opencart/pages/DemoCartPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class DemoCartPage {
+	
+	
+	public DemoCartPage() {
+		System.out.println("demo cart page...");
+	}
+	
+	public void addToCart() {
+		System.out.println("add to cart");
+	}
+}
